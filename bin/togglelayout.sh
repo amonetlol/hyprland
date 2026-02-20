be4b150ed8fe73d584b06338598c7bcbca05872b
amonetlol <65382640+amonetlol@users.noreply.github.com>
2026-02-19 22:03:43 -0300

    Add files via upload

---
+++ bin/togglelayout.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+## Dependência: jq
+if [[ $(hyprctl -j getoption general:layout | jq -r '.str') = "master" ]]; then
+  hyprctl keyword general:layout "dwindle"
+else
+  hyprctl keyword general:layout "master"
+fi
